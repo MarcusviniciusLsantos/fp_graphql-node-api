b0ab2434c047c8a711ed18292b1d188ca05d9629
MarcusviniciusLsantos <marcusvinici25@gmail.com>
2018-02-23 12:42:30 -0300

    Resolvers construction

--- src/graphql/resources/user/user.schema.ts
+++ src/graphql/resources/user/user.schema.ts
@@ -7,6 +7,7 @@ const userTypes = `
           photo: String
           createAt: String!
           updateAt: String!
+          posts(first: Int, offset: Int): [ Post! ]!
       }
 
       input UserCreateInput {
